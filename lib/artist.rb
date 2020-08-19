0c1dc9a8c2c4356e875568984aecd5d6f3339a8f
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-19 04:27:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,14 +13,9 @@ class Artist
   @@artists = []
 
   def initialize
-    # self.class.all << self
     super
     @songs = []
   end
-
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@artists
@@ -35,7 +30,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
